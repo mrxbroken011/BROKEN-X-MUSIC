b292898a7a94d2db7cb9647ccb7e127db7a485f2
Mr Broken <74880754+mrxbroken011@users.noreply.github.com>
2026-01-29 08:26:21 +0530

    testing v2.0.1

--- config.py
+++ config.py
@@ -7,19 +7,19 @@ from pyrogram import filters
 load_dotenv()
 
 # Get this value from my.telegram.org/apps
-API_ID = int(getenv("API_ID") 
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", 35852042)) 
+API_HASH = getenv("API_HASH", None)
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_USERNAME = ("BOT_USERNAME") 
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+BOT_USERNAME = ("BOT_USERNAME", None) 
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 19000))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID"))
+LOGGER_ID = int(getenv("LOGGER_ID", -1002094142057))
 
 # Get this value from @FallenxBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 1841914911))
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/mrxbroken011/BROKENXMUSIC.git",
+    "https://github.com/mrxbroken011/BROKEN-X-MUSIC.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)  # Fill this variable if your upstream repository is private
